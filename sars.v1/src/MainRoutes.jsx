d3fb19cae406df725d03896385fa268436ea6846
Tescoco <AjokeAkinremi43@gmail.com>
2020-09-04 12:14:30 -0800

    redeployed

--- sars.v1/src/MainRoutes.jsx
+++
@@ -1,3 +0,0 @@
-import react from "react";
-import { Route,Switch  } from "react-router-dom";
-import SignUp from "./components/SignUp/SignUp";
--- sars.v1/src/components/SignOut/SignOutField.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SignOutButton from "./SignOutButton"
-
-export default function LayoutTextFields() {
-  return (
-    
-     <SignOutButton />
-   );
-}
